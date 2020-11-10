846052047fbb4f614f1dff5fd06f000edcc889e5
Aluminum-Fairy <miyamoto2525family0731@gmail.com>
2020-11-11 01:52:25 +0900

    バージョン表記更新

--- vpnPacketLogAnalyzerBeta.java
+++ vpnPacketLogAnalyzerBeta.java
@@ -17,7 +17,7 @@ public class vpnPacketLogAnalyzerBeta {
 		File fname;
 		int lineNum, httpLineNum, minS, minE, printLineNum,logTime;
 		boolean bTargetUrl,bUserName,bHttpMethod;
-		final String version = "1.06.0(b00)";
+		final String version = "1.06.0(b01)";
 		ArrayList<String> httplogArr = new ArrayList<String>();
 		ArrayList<ArrayList<String>> httplog = new ArrayList<ArrayList<String>>();
 
